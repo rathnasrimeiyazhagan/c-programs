6c6cbaf1f3f1a188ece43bbc5df5f251d309e4e7
rathnasrimeiyazhagan <rathnasrimeiyazhagan@gmail.com>
2026-01-26 18:40:17 +0530

    Add files via upload

---
+++ no_within_range.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num;
+    scanf("%d", &num);
+
+    if (num >= 10 && num <= 20) {
+        printf("Number is within range");
+    } else {
+        printf("Number is not within range");
+    }
+
+    return 0;
+}
