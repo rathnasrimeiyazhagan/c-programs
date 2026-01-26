e13e9a6d90b78d66b3aa12c1ec7a8da3b1263c96
rathnasrimeiyazhagan <rathnasrimeiyazhagan@gmail.com>
2026-01-26 18:37:14 +0530

    Add files via upload

---
+++ div_byfive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a%5==0){
+        printf("%d is divisible by 5",a);
+    }
+    else{
+        printf("%d is not divisible by 5",a);
+    }
+    return 0;
+   
+}
