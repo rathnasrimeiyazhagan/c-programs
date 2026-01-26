ce197f39d89544d2db853f420da7c21cc38de036
rathnasrimeiyazhagan <rathnasrimeiyazhagan@gmail.com>
2026-01-26 18:38:34 +0530

    Add files via upload

---
+++ grade.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int marks;
+    scanf("%d", &marks);
+
+    if (marks >= 90) {
+        printf("Grade A");
+    }
+    else if (marks >= 75 && marks < 90) {
+        printf("Grade B");
+    }
+    else if (marks >= 50 && marks < 75) {
+        printf("Grade C");
+    }
+    else {
+        printf("Fail");
+    }
+
+    return 0;
+}
