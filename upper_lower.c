a379d5ba0c7ea1663f0521170f7931d4230f5ce9
rathnasrimeiyazhagan <rathnasrimeiyazhagan@gmail.com>
2026-01-26 18:42:25 +0530

    Add files via upload

---
+++ upper_lower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    scanf("%c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z') {
+        printf("Uppercase letter");
+    }
+    else if (ch >= 'a' && ch <= 'z') {
+        printf("Lowercase letter");
+    }
+    else {
+        printf("Not an alphabet");
+    }
+
+    return 0;
+}
