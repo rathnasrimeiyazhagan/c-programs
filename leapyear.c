d77a92d9fbe96578c6c6d020b15967d90cdebbbc
rathnasrimeiyazhagan <rathnasrimeiyazhagan@gmail.com>
2026-01-26 18:39:29 +0530

    Add files via upload

---
+++ leapyear.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a%400==0 || (a%4==0 && a%100!=0)){
+        printf("%d is a leap year",a);
+    }
+    else{
+        printf("%d is not a leap year ",a);
+    }
+    return 0;
+   
+}
