fc5990c0ddd4ce573cb7aaf242001744cc2b9234
rathnasrimeiyazhagan <rathnasrimeiyazhagan@gmail.com>
2026-01-26 18:40:43 +0530

    Add files via upload

---
+++ sim_calci.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    char c;
+    scanf("%d %d %c",&a,&b,&c);
+    switch (c)
+    {
+    case '+':
+        printf("%d",a+b);
+        break;
+    case '-':
+        printf("%d",a-b);
+        break;
+    case '*':
+        printf("%d",a*b);
+        break;
+    case '/':
+        printf("%d",a/b);
+        break;
+    case '%':
+        printf("%d",a%b);
+        break;
+    
+    default:
+    printf("invalid");
+        break;
+    }
+    
+    return 0;
+}
